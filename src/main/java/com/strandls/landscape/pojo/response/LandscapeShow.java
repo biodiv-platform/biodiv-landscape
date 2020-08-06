823b1e0f74d7c27293ffa5c025927ecb7d1a3164
vilay <kandi.vilay09@gmail.com>
2020-08-06 16:00:45 +0530

    Fix : typos

--- src/main/java/com/strandls/landscape/pojo/response/LandscapeShow.java
+++ src/main/java/com/strandls/landscape/pojo/response/LandscapeShow.java
@@ -3,38 +3,43 @@ package com.strandls.landscape.pojo.response;
 import java.util.List;
 
 public class LandscapeShow {
-	
+
 	private String wktData;
 	private List<List<Object>> boundingBox;
-	private TemplateTreeStructure treeStructure;
-	
+	private TemplateTreeStructure contents;
+
 	public LandscapeShow() {
 		super();
 	}
-	
-	public LandscapeShow(String wktData, List<List<Object>> boundingBox2, TemplateTreeStructure treeStructure) {
+
+	public LandscapeShow(String wktData, List<List<Object>> boundingBox, TemplateTreeStructure contents) {
 		super();
 		this.wktData = wktData;
-		this.boundingBox = boundingBox2;
-		this.treeStructure = treeStructure;
+		this.boundingBox = boundingBox;
+		this.contents = contents;
 	}
-	
+
 	public String getWktData() {
 		return wktData;
 	}
+
 	public void setWktData(String wktData) {
 		this.wktData = wktData;
 	}
+
 	public List<List<Object>> getBoundingBox() {
 		return boundingBox;
 	}
+
 	public void setBoundingBox(List<List<Object>> boundingBox) {
 		this.boundingBox = boundingBox;
 	}
-	public TemplateTreeStructure getTreeStructure() {
-		return treeStructure;
+
+	public TemplateTreeStructure getContents() {
+		return contents;
 	}
-	public void setTreeStructure(TemplateTreeStructure treeStructure) {
-		this.treeStructure = treeStructure;
+
+	public void setContents(TemplateTreeStructure contents) {
+		this.contents = contents;
 	}
 }
