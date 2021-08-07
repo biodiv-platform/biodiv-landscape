f2bf4be2863cc648cebc83f884398278af42a744
vishnu <teamdraftbox@gmail.com>
2021-08-07 20:06:13 +0530

    ⚡️ userApi logger incorprated

--- src/main/java/com/strandls/landscape/controller/DownloadLogController.java
+++ src/main/java/com/strandls/landscape/controller/DownloadLogController.java
@@ -16,6 +16,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.pac4j.core.profile.CommonProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.strandls.authentication_utility.filter.ValidateUser;
 import com.strandls.authentication_utility.util.AuthUtil;
@@ -23,6 +25,9 @@ import com.strandls.landscape.ApiConstants;
 import com.strandls.landscape.pojo.DownloadLog;
 import com.strandls.landscape.service.DownloadLogService;
 import com.strandls.landscape.util.UserUtil;
+import com.strandls.user.ApiException;
+import com.strandls.user.controller.UserServiceApi;
+import com.strandls.user.pojo.DownloadLogData;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -31,27 +36,41 @@ import io.swagger.annotations.ApiParam;
 @Path(ApiConstants.DOWNLOAD)
 @Api("DownloadLog")
 public class DownloadLogController {
+	
+	private final Logger logger = LoggerFactory.getLogger(DownloadLogController.class);
 
 	@Inject
 	private DownloadLogService downloadLogService;
-	
+
+	@Inject
+	private UserServiceApi userServiceApi;
+
 	@GET
 	@Path(ApiConstants.PING)
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response ping() {
 		return Response.status(Status.OK).entity("PONG").build();
 	}
-	
+
 	@POST
 	@Path("log")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@ApiOperation(value = "Add the download log ", response = DownloadLog.class)
 	public Response saveDownloadLog(@ApiParam(name = "downloadLog") DownloadLog downloadLog) {
-		downloadLog = downloadLogService.save(downloadLog);
-		return Response.ok().entity(downloadLog).build();
+		DownloadLogData data = new DownloadLogData();
+		data.setFilePath(downloadLog.getFilePath());
+		data.setFileType(downloadLog.getType());
+		data.setFilterUrl(downloadLog.getFilterUrl());
+		data.setStatus(downloadLog.getStatus());
+		data.setSourcetype("Lanscapoe");
+		try {
+			return Response.ok().entity(userServiceApi.logDocumentDownload(data)).build();
+		} catch (ApiException e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
 	}
-	
+
 	@GET
 	@Path("log/{autherId}")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -59,16 +78,16 @@ public class DownloadLogController {
 	@ApiOperation(value = "Get the download log by autherId", response = DownloadLog.class)
 	@ValidateUser
 	public Response getDownloadLog(@Context HttpServletRequest request, @PathParam("autherId") Long autherId) {
-		
+
 		CommonProfile profile = AuthUtil.getProfileFromRequest(request);
-		if(!UserUtil.isAdmin(request) && !autherId.toString().equals(profile.getId())) {
+		if (!UserUtil.isAdmin(request) && !autherId.toString().equals(profile.getId())) {
 			return Response.status(Status.UNAUTHORIZED).build();
 		}
-		
+
 		List<DownloadLog> downloadLog = downloadLogService.getDownloadLogByAutherId(autherId);
 		return Response.ok().entity(downloadLog).build();
 	}
-	
+
 	@GET
 	@Path("log/all")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -76,7 +95,7 @@ public class DownloadLogController {
 	@ApiOperation(value = "Get all the download log ", response = DownloadLog.class, responseContainer = "List")
 	@ValidateUser
 	public Response getAllDownloadLog(@Context HttpServletRequest request) {
-		if(!UserUtil.isAdmin(request)) {
+		if (!UserUtil.isAdmin(request)) {
 			return Response.status(Status.UNAUTHORIZED).build();
 		}
 		List<DownloadLog> downloadLogs = downloadLogService.getAllDownloadLogs();
